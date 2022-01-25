2fbfe7d5363195d1e2decb486a722351a64ae0d8
Codevelle Alexis <codevellea@a11p16.fil.univ-lille1.fr>
2022-01-25 17:10:48 +0100

    Begin of the structure of the clas

---
+++ lib/Network.py
@@ -0,0 +1,21 @@
+
+
+class Network:
+
+    def __init__(self, input_data):
+        pass
+
+    def predict(self):
+        pass
+
+    def train(self):
+        pass
+
+    def softmax(self):
+        pass
+
+    def relu(self):
+        pass
+
+    def backward_propagation(self):
+        pass
